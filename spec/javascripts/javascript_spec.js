f33246283b3b99efdcabf5d3a7f7276ddaeb5eff
notryanb <notryanb@gmail.com>
2015-03-24 21:34:36 -0400

    Implemented first test for errorModule

--- spec/javascripts/javascript_spec.js
+++ spec/javascripts/javascript_spec.js
@@ -9,36 +9,50 @@ describe("Calculator function test", function() {
     expect(calculator).toBeDefined()
   })
 
-  it("should have an add function", function() {
-    expect(calculator.add).toBeDefined()
-  })
+  describe("Add Function Test", function() {
+    it("should have an add function", function() {
+      expect(calculator.add).toBeDefined()
+    })
 
-  it("should add correctly", function() {
-    expect(calculator.add(10,5)).toBe(15)
-  })
+    it("should add correctly", function() {
+      expect(calculator.add(10,5)).toBe(15)
+    })
+  });
 
-  it("should have an subtract function", function() {
-    expect(calculator.subtract).toBeDefined()
-  })
+  describe("Subtract Function Test", function() {
+    it("should have an subtract function", function() {
+      expect(calculator.subtract).toBeDefined()
+    })
 
-  it("should subtract correctly", function() {
-    expect(calculator.subtract(10,5)).toBe(5)
-  })
+    it("should subtract correctly", function() {
+      expect(calculator.subtract(10,5)).toBe(5)
+    })
+  });
 
-  it("should have an multiply function", function() {
-    expect(calculator.multiply).toBeDefined()
-  })
+  describe("Multiply Function Test", function() {
+    it("should have an multiply function", function() {
+      expect(calculator.multiply).toBeDefined()
+    })
 
-  it("should multiply correctly", function() {
-    expect(calculator.multiply(10,5)).toBe(50)
-  })
+    it("should multiply correctly", function() {
+      expect(calculator.multiply(10,5)).toBe(50)
+    })
+  });
 
-  it("should have an divide function", function() {
-    expect(calculator.divide).toBeDefined()
-  })
+  describe("Divide Function Test", function() {
+    it("should have an divide function", function() {
+      expect(calculator.divide).toBeDefined()
+    })
 
-  it("should divide correctly", function() {
-    expect(calculator.divide(10,5)).toBe(2)
+    it("should divide correctly", function() {
+      expect(calculator.divide(10,5)).toBe(2)
+    })
+  });
+});
+
+describe("errorModule function", function() {
+  it("should be defined", function() {
+    expect(errorModule).toBeDefined()
   })
 
 
